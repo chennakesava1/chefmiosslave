ffbc5e027034f2c9dc5f2727e6af6efbba87da11
chennakesava1 <chennakesava1@users.noreply.github.com>
2016-07-11 15:01:56 +0530

    change on SSH

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,8 +5,12 @@ default['maven']['settings_source'] = 'http://nexus.wdf.sap.corp:8081/nexus/cont
 
 default['maven']['maven_Settings_Security_source'] = 'file:///sapmnt/production/info/make/pamber/IOS/settings-security.xml'
 
-default['maven']['maveneventspyclient_sorce'] = 'http://nexus.wdf.sap.corp:8081/nexus/content/repositories/build.milestones/com/sap/prd/BuildCounterSlave/BuildCounterSlave/0.0.1/BuildCounterSlave-0.0.1.jar'
+default['maven']['maveneventspyclient_source'] = 'http://nexus.wdf.sap.corp:8081/nexus/content/repositories/build.milestones/com/sap/prd/BuildCounterSlave/BuildCounterSlave/0.0.1/BuildCounterSlave-0.0.1.jar'
 default['maven']['maveneventspyclient_destination'] = "#{default['maven']['extract_path']}/lib/ext/BuildCounterSlave.jar"
 
 default['maven']['eventspyclientconfig_source'] = 'file:///sapmnt/production/swl/iOS/Repositories/build.milestones/com.sap.prd.maveneventspyclient/installer-maveneventspy-client-conf/1.0.0/installer-maveneventspy-client-conf-1.0.0.properties'
 default['maven']['eventspyclientconfig_destination'] = "#{ENV['HOME']}/mvn_event_spy_slave.properties"
+
+
+default['sshKeysVelocity']['sorces'] = 'file:///sapmnt/production/swl/iOS/Repositories/build.milestones/com.sap.prd.mobile.ios.buildenv/git_ssh_known_hosts/1.2/git_ssh_known_hosts-1.2'
+
